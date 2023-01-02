xepa/puppetlabs-apt

commit cb6e58cedbd6d7a9bfc63c97c83d51eb39e9c7dc

(GH-cat-9) Update module to match current syntax standard
Module is now in compliance with the following rules:
- optional_default
- strict_indent
- unquoted_string_in_case
- parameter_documentation
- relative_classname_inclusion
- no-top_scope_facts-check
- no-top_scope_variable-check
- variable_scope

The below exception has been left in place:
- disable_anchor_resource

manifests/update.pp
@@ -5,12 +5,12 @@
 class apt::update {
   assert_private()
 
-  #TODO: to catch if $::apt_update_last_success has the value of -1 here. If we
+  #TODO: to catch if $apt_update_last_success has the value of -1 here. If we
   #opt to do this, a info/warn would likely be all you'd need likely to happen
   #on the first run, but if it's not run in awhile something is likely borked
   #with apt and we'd want to know about it.
 
-  case $::apt::_update['frequency'] {
+  case $apt::_update['frequency'] {
     'always': {
       $_kick_apt = true
     }
@@ -18,8 +18,8 @@ class apt::update {
       #compare current date with the apt_update_last_success fact to determine
       #if we should kick apt_update.
       $daily_threshold = (Integer(Timestamp().strftime('%s')) - 86400)
-      if $::apt_update_last_success {
-        if $::apt_update_last_success + 0 < $daily_threshold {
+      if $apt::apt_update_last_success {
+        if $apt::apt_update_last_success + 0 < $daily_threshold {
           $_kick_apt = true
         } else {
           $_kick_apt = false
@@ -33,8 +33,8 @@ class apt::update {
       #compare current date with the apt_update_last_success fact to determine
       #if we should kick apt_update.
       $weekly_threshold = (Integer(Timestamp().strftime('%s')) - 604800)
-      if $::apt_update_last_success {
-        if ( $::apt_update_last_success + 0 < $weekly_threshold ) {
+      if $apt::apt_update_last_success {
+        if ( $apt::apt_update_last_success + 0 < $weekly_threshold ) {
           $_kick_apt = true
         } else {
           $_kick_apt = false
@@ -57,12 +57,12 @@ class apt::update {
     $_refresh = true
   }
   exec { 'apt_update':
-    command     => "${::apt::provider} update",
-    loglevel    => $::apt::_update['loglevel'],
+    command     => "${apt::provider} update",
+    loglevel    => $apt::_update['loglevel'],
     logoutput   => 'on_failure',
     refreshonly => $_refresh,
-    timeout     => $::apt::_update['timeout'],
-    tries       => $::apt::_update['tries'],
+    timeout     => $apt::_update['timeout'],
+    tries       => $apt::_update['tries'],
     try_sleep   => 1,
   }
 }

spec/classes/apt_update_spec.rb
@@ -9,7 +9,7 @@ describe 'apt::update', type: :class do
       'we are due for a run'                         => 1_406_660_561,
       'the update-success-stamp file does not exist' => -1,
     }.each_pair do |desc, factval|
-      context "when $::apt_update_last_success indicates #{desc}" do
+      context "when $apt_update_last_success indicates #{desc}" do
         let(:facts) do
           {
             os: {
@@ -24,7 +24,7 @@ describe 'apt::update', type: :class do
                 id: 'Debian',
               },
             },
-            apt_update_last_success: factval,
+            'apt::apt_update_last_success': factval,
           }
         end
         let(:pre_condition) do
@@ -37,7 +37,7 @@ describe 'apt::update', type: :class do
         end
       end
     end
-    context 'when $::apt_update_last_success is nil' do
+    context 'when $apt_update_last_success is nil' do
       let(:facts) do
         {
           os: {
@@ -76,7 +76,7 @@ describe 'apt::update', type: :class do
               id: 'Debian',
             },
           },
-          apt_update_last_success: Time.now.to_i,
+          'apt::apt_update_last_success': Time.now.to_i,
         }
       end
       let(:pre_condition) do
@@ -98,7 +98,7 @@ describe 'apt::update', type: :class do
       'we are due for a run'                         => 1_406_660_561,
       'the update-success-stamp file does not exist' => -1,
     }.each_pair do |desc, factval|
-      context "when $::apt_update_last_success indicates #{desc}" do
+      context "when $apt_update_last_success indicates #{desc}" do
         let(:facts) do
           {
             os: {
@@ -113,7 +113,7 @@ describe 'apt::update', type: :class do
                 id: 'Debian',
               },
             },
-            apt_update_last_success: factval,
+            'apt::apt_update_last_success': factval,
           }
         end
         let(:pre_condition) { "class{ '::apt': update => {'frequency' => 'reluctantly' },}" }
@@ -124,7 +124,7 @@ describe 'apt::update', type: :class do
         end
       end
     end
-    context 'when $::apt_update_last_success is nil' do
+    context 'when $apt_update_last_success is nil' do
       let(:facts) do
         {
           os: {
@@ -152,7 +152,7 @@ describe 'apt::update', type: :class do
   ['daily', 'weekly'].each do |update_frequency|
     context "when apt::update['frequency'] has the value of #{update_frequency}" do
       { 'we are due for a run' => 1_406_660_561, 'the update-success-stamp file does not exist' => -1 }.each_pair do |desc, factval|
-        context "when $::apt_update_last_success indicates #{desc}" do
+        context "when $apt_update_last_success indicates #{desc}" do
           let(:facts) do
             {
               os: {
@@ -167,7 +167,7 @@ describe 'apt::update', type: :class do
                   id: 'Debian',
                 },
               },
-              apt_update_last_success: factval,
+              'apt::apt_update_last_success': factval,
             }
           end
           let(:pre_condition) { "class{ '::apt': update => {'frequency' => '#{update_frequency}',} }" }
@@ -178,7 +178,7 @@ describe 'apt::update', type: :class do
           end
         end
       end
-      context 'when the $::apt_update_last_success fact has a recent value' do
+      context 'when the $apt_update_last_success fact has a recent value' do
         let(:facts) do
           {
             os: {
@@ -193,7 +193,7 @@ describe 'apt::update', type: :class do
                 id: 'Debian',
               },
             },
-            apt_update_last_success: Time.now.to_i,
+            'apt::apt_update_last_success': Time.now.to_i,
           }
         end
         let(:pre_condition) { "class{ '::apt': update => {'frequency' => '#{update_frequency}',} }" }
@@ -203,7 +203,7 @@ describe 'apt::update', type: :class do
           is_expected.to contain_exec('apt_update').with('refreshonly' => true)
         end
       end
-      context 'when $::apt_update_last_success is nil' do
+      context 'when $apt_update_last_success is nil' do
         let(:facts) do
           {
             os: {
@@ -218,7 +218,7 @@ describe 'apt::update', type: :class do
                 id: 'Debian',
               },
             },
-            apt_update_last_success: nil,
+            'apt::apt_update_last_success': nil,
           }
         end
         let(:pre_condition) { "class{ '::apt': update => {'frequency' => '#{update_frequency}',} }" }

.puppet-lint.rc
@@ -1,9 +1,2 @@
 --relative
---no-top_scope_facts-check
---no-topscope_variable-check
---no-relative_classname_inclusion-check
---no-parameter_documentation-check
 --no-anchor_resource-check
---no-strict_indent-check
---no-unquoted_string_in_case-check
---no-optional_default-check

.sync.yml
@@ -33,11 +33,4 @@ spec/spec_helper.rb:
 changelog_since_tag: '5.0.1'
 Rakefile:
   extra_disabled_lint_checks:
-    - top_scope_facts
-    - topscope_variable
-    - relative_classname_inclusion
-    - parameter_documentation
     - anchor_resource
-    - strict_indent
-    - unquoted_string_in_case
-    - optional_default

Rakefile
@@ -42,15 +42,7 @@ def changelog_future_release
 end
 
 PuppetLint.configuration.send('disable_relative')
-PuppetLint.configuration.send('disable_top_scope_facts')
-PuppetLint.configuration.send('disable_topscope_variable')
-PuppetLint.configuration.send('disable_relative_classname_inclusion')
-PuppetLint.configuration.send('disable_parameter_documentation')
 PuppetLint.configuration.send('disable_anchor_resource')
-PuppetLint.configuration.send('disable_strict_indent')
-PuppetLint.configuration.send('disable_unquoted_string_in_case')
-PuppetLint.configuration.send('disable_optional_default')
-
 
 if Bundler.rubygems.find_name('github_changelog_generator').any?
   GitHubChangelogGenerator::RakeTask.new :changelog do |config|

manifests/backports.pp
@@ -76,7 +76,7 @@ class apt::backports (
     }
   }
   unless $location {
-    $_location = $::apt::backports['location']
+    $_location = $apt::backports['location']
   }
   unless $release {
     if fact('os.distro.codename') {
@@ -86,10 +86,10 @@ class apt::backports (
     }
   }
   unless $repos {
-    $_repos = $::apt::backports['repos']
+    $_repos = $apt::backports['repos']
   }
   unless $key {
-    $_key =  $::apt::backports['key']
+    $_key =  $apt::backports['key']
   }
 
   if $pin =~ Hash {

manifests/init.pp
@@ -55,6 +55,9 @@
 # @option update [Integer] :tries
 #    Specifies how many times to retry the update after receiving a DNS or HTTP error. Default: undef.
 #
+# @param update_defaults
+#   The default update settings that are combined and merged with the passed `update` value
+#
 # @param purge
 #   Specifies whether to purge any existing settings that aren't managed by Puppet. Valid options: a hash made up from the following keys:
 #
@@ -70,8 +73,14 @@
 # @option purge [Boolean] :preferences.d.
 #   Specifies whether to purge any unmanaged entries from preferences.d. Default false.
 #
+# @param purge_defaults
+#   The default purge settings that are combined and merged with the passed `purge` value
+#
 # @param proxy
 #   Configures Apt to connect to a proxy server. Valid options: a hash matching the locally defined type apt::proxy.
+#
+# @param proxy_defaults
+#   The default proxy settings that are combined and merged with the passed `proxy` value
 #
 # @param sources
 #   Creates new `apt::source` resources. Valid options: a hash to be passed to the create_resources function linked above.
@@ -124,6 +133,14 @@
 #
 # @param sources_list_force
 #   Specifies whether to perform force purge or delete. Default false.
+#
+# @param include_defaults
+#
+# @param apt_conf_d
+#   The path to the file `apt.conf.d`
+#
+# @param source_key_defaults
+#   The fault `source_key` settings
 #
 class apt (
   Hash $update_defaults                           = $apt::params::update_defaults,
@@ -183,8 +200,8 @@ class apt (
     assert_type(Integer, $update['tries'])
   }
 
-  $_update = merge($::apt::update_defaults, $update)
-  include ::apt::update
+  $_update = merge($apt::update_defaults, $update)
+  include apt::update
 
   if $purge['sources.list'] {
     assert_type(Boolean, $purge['sources.list'])
@@ -205,24 +222,28 @@ class apt (
     assert_type(Boolean, $purge['apt.conf.d'])
   }
 
-  $_purge = merge($::apt::purge_defaults, $purge)
+  $_purge = merge($apt::purge_defaults, $purge)
 
   if $proxy['perhost'] {
     $_perhost = $proxy['perhost'].map |$item| {
       $_item = merge($apt::proxy_defaults, $item)
       $_scheme = $_item['https'] ? {
         true    => 'https',
-        default => 'http' }
+        default => 'http',
+      }
       $_port = $_item['port'] ? {
         Integer => ":${_item['port']}",
         default => ''
       }
       $_target = $_item['direct'] ? {
         true    => 'DIRECT',
-        default => "${_scheme}://${_item['host']}${_port}/" }
+        default => "${_scheme}://${_item['host']}${_port}/",
+      }
       merge($item, {
-        'scheme' => $_scheme,
-        'target' => $_target })
+          'scheme' => $_scheme,
+          'target' => $_target,
+        }
+      )
     }
   } else {
     $_perhost = {}
@@ -260,7 +281,7 @@ class apt (
       true    => "# Repos managed by puppet.\n",
       default => undef,
     }
-    }
+  }
 
   $preferences_ensure = $_purge['preferences'] ? {
     true    => absent,
@@ -274,7 +295,7 @@ class apt (
 
   file { 'sources.list':
     ensure  => $sources_list_ensure,
-    path    => $::apt::sources_list,
+    path    => $apt::sources_list,
     owner   => root,
     group   => root,
     content => $sources_list_content,
@@ -283,7 +304,7 @@ class apt (
 
   file { 'sources.list.d':
     ensure  => directory,
-    path    => $::apt::sources_list_d,
+    path    => $apt::sources_list_d,
     owner   => root,
     group   => root,
     purge   => $_purge['sources.list.d'],
@@ -293,7 +314,7 @@ class apt (
 
   file { 'preferences':
     ensure => $preferences_ensure,
-    path   => $::apt::preferences,
+    path   => $apt::preferences,
     owner  => root,
     group  => root,
     notify => Class['apt::update'],
@@ -301,7 +322,7 @@ class apt (
 
   file { 'preferences.d':
     ensure  => directory,
-    path    => $::apt::preferences_d,
+    path    => $apt::preferences_d,
     owner   => root,
     group   => root,
     purge   => $_purge['preferences.d'],
@@ -311,7 +332,7 @@ class apt (
 
   file { 'apt.conf.d':
     ensure  => directory,
-    path    => $::apt::apt_conf_d,
+    path    => $apt::apt_conf_d,
     owner   => root,
     group   => root,
     purge   => $_purge['apt.conf.d'],

manifests/key.pp
@@ -40,10 +40,10 @@ define apt::key (
   Enum['present', 'absent', 'refreshed'] $ensure                                                                        = present,
   Optional[String] $content                                                                                             = undef,
   Optional[Pattern[/\Ahttps?:\/\//, /\Aftp:\/\//, /\A\/\w+/]] $source                                                   = undef,
-  Pattern[/\A((hkp|hkps|http|https):\/\/)?([a-z\d])([a-z\d-]{0,61}\.)+[a-z\d]+(:\d{2,5})?(\/[a-zA-Z\d\-_.]+)*\/?$/] $server = $::apt::keyserver,
+  Pattern[/\A((hkp|hkps|http|https):\/\/)?([a-z\d])([a-z\d-]{0,61}\.)+[a-z\d]+(:\d{2,5})?(\/[a-zA-Z\d\-_.]+)*\/?$/] $server = $apt::keyserver,
   Boolean $weak_ssl                                                                                                     = false,
-  Optional[String] $options                                                                                             = $::apt::key_options,
-  ) {
+  Optional[String] $options                                                                                             = $apt::key_options,
+) {
   case $ensure {
     /^(refreshed|present)$/: {
       if defined(Anchor["apt_key ${id} absent"]) {
@@ -82,7 +82,7 @@ define apt::key (
       }
     }
 
-    absent: {
+    /^absent$/: {
       if defined(Anchor["apt_key ${id} present"]) {
         fail("key with id ${id} already ensured as present")
       }

commit bacf745929fe3f018f1108e64249140e5337d83f

fix: remove `apt::` prefix from fact variables
Fact variables are top-scopped and should not be prefixed by module name.

This bug was introduced by cb6e58cedbd6d7a9bfc63c97c83d51eb39e9c7dc.

manifests/update.pp
@@ -5,7 +5,7 @@
 class apt::update {
   assert_private()
 
-  #TODO: to catch if $apt_update_last_success has the value of -1 here. If we
+  #TODO: to catch if apt_update_last_success has the value of -1 here. If we
   #opt to do this, a info/warn would likely be all you'd need likely to happen
   #on the first run, but if it's not run in awhile something is likely borked
   #with apt and we'd want to know about it.
@@ -18,8 +18,8 @@ class apt::update {
       #compare current date with the apt_update_last_success fact to determine
       #if we should kick apt_update.
       $daily_threshold = (Integer(Timestamp().strftime('%s')) - 86400)
-      if $apt::apt_update_last_success {
-        if $apt::apt_update_last_success + 0 < $daily_threshold {
+      if $facts['apt_update_last_success'] {
+        if $facts['apt_update_last_success'] + 0 < $daily_threshold {
           $_kick_apt = true
         } else {
           $_kick_apt = false
@@ -33,8 +33,8 @@ class apt::update {
       #compare current date with the apt_update_last_success fact to determine
       #if we should kick apt_update.
       $weekly_threshold = (Integer(Timestamp().strftime('%s')) - 604800)
-      if $apt::apt_update_last_success {
-        if ( $apt::apt_update_last_success + 0 < $weekly_threshold ) {
+      if $facts['apt_update_last_success'] {
+        if $facts['apt_update_last_success'] + 0 < $weekly_threshold {
           $_kick_apt = true
         } else {
           $_kick_apt = false

spec/classes/apt_update_spec.rb
@@ -24,7 +24,7 @@ describe 'apt::update', type: :class do
                 id: 'Debian',
               },
             },
-            'apt::apt_update_last_success': factval,
+            'apt_update_last_success': factval,
           }
         end
         let(:pre_condition) do
@@ -76,7 +76,7 @@ describe 'apt::update', type: :class do
               id: 'Debian',
             },
           },
-          'apt::apt_update_last_success': Time.now.to_i,
+          'apt_update_last_success': Time.now.to_i,
         }
       end
       let(:pre_condition) do
@@ -113,7 +113,7 @@ describe 'apt::update', type: :class do
                 id: 'Debian',
               },
             },
-            'apt::apt_update_last_success': factval,
+            'apt_update_last_success': factval,
           }
         end
         let(:pre_condition) { "class{ '::apt': update => {'frequency' => 'reluctantly' },}" }
@@ -167,7 +167,7 @@ describe 'apt::update', type: :class do
                   id: 'Debian',
                 },
               },
-              'apt::apt_update_last_success': factval,
+              'apt_update_last_success': factval,
             }
           end
           let(:pre_condition) { "class{ '::apt': update => {'frequency' => '#{update_frequency}',} }" }
@@ -193,7 +193,7 @@ describe 'apt::update', type: :class do
                 id: 'Debian',
               },
             },
-            'apt::apt_update_last_success': Time.now.to_i,
+            'apt_update_last_success': Time.now.to_i,
           }
         end
         let(:pre_condition) { "class{ '::apt': update => {'frequency' => '#{update_frequency}',} }" }
@@ -218,7 +218,7 @@ describe 'apt::update', type: :class do
                 id: 'Debian',
               },
             },
-            'apt::apt_update_last_success': nil,
+            'apt_update_last_success': nil,
           }
         end
         let(:pre_condition) { "class{ '::apt': update => {'frequency' => '#{update_frequency}',} }" }